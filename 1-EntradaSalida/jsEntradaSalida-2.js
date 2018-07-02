f91f11911adc05e7d4834e6f664c287e691e4227
Lautaro Galarza <40763633+lutaro@users.noreply.github.com>
2018-07-02 12:06:26 -0300

    ejercicios 1 2 y 3
    curso de ingreso clase 1

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,15 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+
+
+//comentario de linea var person = prompt("Please enter your name", "Harry Potter");
+
+var nombre;
+
+nombre =prompt("ingrese su nombre", "lautaro");
+
+alert("su nombre es " +nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,18 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+
+/*
+	alert("ejercicio 3");
+		console.log("esto esta bien")
+*/
+var nombre;
+nombre=document.getElementById('elNombre').value;
+alert(nombre);
+
+
+
 }
 
 
